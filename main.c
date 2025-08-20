78da1d9dd99060f7ada638cc161ab7e1ce3ce54c
Phantom Coder <abdulahad474783@gmail.com>
2025-08-20 15:10:10 +0600

    All methods

---
+++ main.c
@@ -0,0 +1,93 @@
+// Simple
+#include <stdio.h>
+int main()
+{
+    int a,b,c,sum;
+    printf ("Enter three integer: ");
+    scanf ("%d %d %d",&a,&b,&c);
+    sum=a+b+c;
+    printf ("Sum of three numbers: %d",sum);
+    return 0;
+}
+// By using function (Integer)
+#include <stdio.h>
+int add (int a, int b, int c)
+{
+    return a+b+c;
+}
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d",&a,&b,&c);
+    printf ("Sum of three numbers: %d",add(a,b,c));
+    return 0;
+}
+// By using function (float)
+#include <stdio.h>
+float add (float a, float b, float c)
+{
+    return a+b+c;
+}
+int main()
+{
+    float a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%f %f %f",&a,&b,&c);
+    printf ("Sum of three numbers: %.2f",add(a,b,c));
+    return 0;
+}
+// By using Structure data type
+#include <stdio.h>
+struct add
+{
+    float value;
+};
+int main()
+{
+    int i,n;
+    float sum=0.0;
+    printf ("Enter limit of input numbers: ");
+    scanf ("%d",&n);
+    struct add s[n];
+    printf ("Enter %d numbers: ",n);
+    for (i=0; i<n; i++)
+    {
+        scanf ("%f",&s[i].value);
+        sum+=s[i].value;
+    }
+    printf ("The sum of three numbers: %.2f",sum);
+    return 0;
+}
+// File
+#include <stdio.h>
+int main()
+{
+    FILE *file1,*file2;
+    float a,b,c,sum;
+    char ch;
+    file1 = fopen ("Sum.txt","w");
+    file2 = fopen ("Sum.txt","r");
+    if (file1==NULL)
+        printf ("File does not exists.");
+    else
+    {
+        printf ("Enter three numbers: ");
+        scanf ("%f %f %f",&a,&b,&c);
+        sum=a+b+c;
+        fprintf (file1,"The sum of three number is = %.2f",sum);
+        fclose (file1);
+    }
+    if (file2==NULL)
+        printf ("File does not exists");
+    else
+    {
+        while (!feof(file2))
+        {
+            ch = fgetc (file2);
+            printf ("%c",ch);
+        }
+        fclose (file2);
+    }
+    return 0;
+}
